21172394ec9f46f919826316a5caf5e66039d889
asadi80 <a8sadi@gmail.com>
2021-11-09 18:43:52 -0800

    generating the password

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,48 @@ var numbersArray = numbers.split("");
 var symbols = "!#$%&\()*+,-./:;<=>?@^[\\]^_`{|}~";
 var symbolsArray = symbols.split("");
 
+var allCharsArry = [];
+  var password = ""; 
+  
+  var TotLength = prompt("How many characters would you like your password to be? Choose betwhwn 8 up to 128");
+  if(!TotLength){
+    alert("This needs a value\nPlease start over.");
+    
+  }
+  else if(TotLength <8 || TotLength > 128){
+      alert("It is recommended to have a password between 8 and 128 characters long!\nPlease start over.");
+      
+
+    }
+    else{
+      if(confirm("Would you like your password to contain upper case letters?")){
+          Array.prototype.push.apply(allCharsArry, upperCaseArray);
+      }
+
+      if(confirm("Would you like your password to contain lower case letters?")){
+          Array.prototype.push.apply(allCharsArry, lowerCaseArray);
+      }
+
+      if(confirm("Would you like your password to contain numbers?")){
+          Array.prototype.push.apply(allCharsArry, numbersArray);
+      }
+
+      if(confirm("Would you like your password to contain symbols?")){
+          Array.prototype.push.apply(allCharsArry, symbolsArray);
+      }
+
+      if(allCharsArry.length===0){
+          alert("You must select at lease 1 type of characters to generate a password!\nPlease start over.");
+      }
+      else{
+        for(var i=0; i<TotLength; i++){
+            var random = Math.floor(Math.random()*allCharsArry.length);
+            password += allCharsArry[random];
+        }
+      }
+
+    }
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
